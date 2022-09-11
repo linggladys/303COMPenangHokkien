8e2752adb5d63b90c97c12e9b93293c3a017d356
linggladys <ygladysling@gmail.com>
2022-09-11 22:57:16 +0800

    Revert "Revert "add change password page and modified user profile""
    This reverts commit 78ecde9d62bf6e26c97ad79e7b4f7dada8624731.

--- resources/views/user/profile/change-password.blade.php
+++ resources/views/user/profile/change-password.blade.php
@@ -1,9 +1,49 @@
 @extends('layouts.app')
 @section('title', 'User Profile Password Change')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <h1 class="display-6">Password</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('profile.updatePassword') }}">
+                        @csrf
+                        <h5 class="card-title fw-bolder">
+                            User Password Change Page
+                        </h5>
+                        @if ($errors->any())
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <i class="fa-solid fa-face-frown" style="font-size: 24pt"></i>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+
+                        <div class="mb-3">
+                            <label for="old_password" class="form-label">Current Password</label>
+                            <input type="password" class="form-control" id="old_password" name="old_password" value="{{ old('old_password') }}">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="new_password" class="form-label">New Password</label>
+                            <input type="password" class="form-control" id="new_password" name="new_password">
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label for="confirm_password" class="form-label">Reconfirm New Password</label>
+                            <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+                        </div>
+
+                        <div class="d-flex justify-content-center gap-2">
+                            <button type="submit" class="btn btn-primary">Update Password</button>
+                            <a href="{{ route('profile.index') }}" class="btn btn-danger">Cancel</a>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -8,6 +8,12 @@
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
+        @elseif(session('failures'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <i class="fa-solid fa-face-frown" style="font-size: 18pt"></i>
+            {{ session('failures') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     @endif
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -18,7 +24,7 @@
                                 class="w-25 img-fluid img-thumbnail">
                         @else
                             <img src="{{ asset('assets/images/user.png') }}" alt="user-profile-image"
-                                class="img-fluid rounded-circle">
+                                class="img-fluid img-thumbnail">
                         @endif
                     </div>
 
