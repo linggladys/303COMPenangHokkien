8ac238469bb057e5fa6e5cbd29e6c53d573775ca
linggladys <ygladysling@gmail.com>
2022-09-22 21:51:17 +0800

    changed from Lesson to PhraseCategory

---
+++ resources/views/phraseCategory/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Phrases')
+@section('content')
+<div class="container">
+    <div class="row">
+        <x-page-header>Phrase Categories</x-page-header>
+
+    </div>
+</div>
+@endsection
