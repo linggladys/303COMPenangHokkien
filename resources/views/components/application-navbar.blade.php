7ed9da99984de8eca5e7999b6d5fd99e150addaa
linggladys <ygladysling@gmail.com>
2022-09-23 22:45:15 +0800

    add dark mode

--- resources/views/components/application-navbar.blade.php
+++ resources/views/components/application-navbar.blade.php
@@ -1,60 +1,77 @@
 <div>
     @auth
-    <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}">
-                <img src="{{ asset('assets/logos/logo.png') }}" alt="logo" class="navbar-application-logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ asset('assets/logos/logo.png') }}" alt="logo" class="navbar-application-logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-                    <!-- Some WIP Navbars -->
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-indigo-700">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('phrasesCategory.index') }}" class="nav-link text-indigo-700">Phrases</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a href="{{ url('games') }}" class="nav-link text-indigo-700">Games</a>
-                    </li>
-                    {{-- <li class="nav-item disabled">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        <!-- Some WIP Navbars -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-indigo-700">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('phrasesCategory.index') }}" class="nav-link text-indigo-700">Phrases</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="{{ url('games') }}" class="nav-link text-indigo-700">Games</a>
+                        </li>
+                        {{-- <li class="nav-item disabled">
                         <a href="#" class="nav-link">Q-n-a Pronunciation</a>
                     </li> --}}
-                </ul>
+                    </ul>
 
-                <!-- Right Side Of Navbar -->
+                    <!-- Right Side Of Navbar -->
 
-                <ul class="navbar-nav ms-auto">
-                    @if (Auth::user()->profile_image)
-                            <img src="{{ asset('uploads/user_images/' .Auth::user()->profile_image) }}" alt="user-image-profile"
-                                class="icon-avatar">
+                    <ul class="navbar-nav ms-auto">
+                        @if (Auth::user()->profile_image)
+                            <img src="{{ asset('uploads/user_images/' . Auth::user()->profile_image) }}"
+                                alt="user-image-profile" class="icon-avatar">
                         @else
                             <img src="{{ asset('assets/images/user.png') }}" alt="user-profile-image"
                                 class="icon-avatar rounded-circle">
                         @endif
-                    <!-- Authentication Links -->
+                        <!-- Authentication Links -->
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link text-indigo-700 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link text-indigo-700 dropdown-toggle" href="#"
+                                role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->username }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-end bg-light" aria-labelledby="navbarDropdown">
+
+
+                            <div class="dropdown-menu dropdown-menu-end bg-light" style="margin: 0" aria-labelledby="navbarDropdown">
+                                <a href="#" class="dropdown-iem text-indigo-700">
+                                    <div class="form-check form-switch" id="custom-form-switch">
+                                        <input type="checkbox" class="form-check-input" id="darkSwitch" />
+                                        <label class="custom-control-label" for="darkSwitch">Dark Mode
+                                            <i class="fa fa-moon"></i>
+                                        </label>
+                                    </div>
+                                </a>
+                                <hr>
+
+
                                 {{-- Redirect to User Profile --}}
                                 <a class="dropdown-item text-indigo-700" href="{{ route('profile.index') }}">
                                     <i class="fa-solid fa-user"></i>
                                     {{ __('Profile') }}
                                 </a>
 
+
                                 {{-- Redirect to logout --}}
                                 <a class="dropdown-item text-indigo-700" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                                 <i class="fa-solid fa-sign-out-alt"></i>
+                                    <i class="fa-solid fa-sign-out-alt"></i>
                                     {{ __('Logout') }}
                                 </a>
 
@@ -67,10 +84,10 @@
                         </li>
 
 
-                </ul>
-            </div>
+                    </ul>
+                </div>
 
-        </div>
-    </nav>
+            </div>
+        </nav>
     @endauth
 </div>
