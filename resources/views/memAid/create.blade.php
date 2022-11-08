56a20a6ea16f807d15dac989fbcf763c5ca60c8d
linggladys <ygladysling@gmail.com>
2022-11-08 23:39:34 +0800

    small modifications 2

--- resources/views/memAid/create.blade.php
+++ resources/views/memAid/create.blade.php
@@ -4,6 +4,16 @@
     <div class="container">
         <x-app-page-header>Create a Memory Aid for {{ $phrase->phrase_name }}</x-app-page-header>
         <div class="row">
+            @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                @foreach ($errors->all() as $error)
+                <ul>
+                  <li>{{ $error }}</li>
+                </ul>
+                @endforeach
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
             <div class="col-md-12">
                 <form action="{{ route('memaid.store',$phrase->id) }}" method="POST">
                     @csrf
